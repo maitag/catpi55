1814113af2782caadb3f8f8ca38f30afdcbc50e6
maitag <info@maitag.de>
2026-07-11 20:08:41 +0200

    preparing more for actor-sync

--- src/automat/actor/ActorSim.hx
+++ src/automat/actor/ActorSim.hx
@@ -7,8 +7,8 @@ class ActorSim {
 		if ( a.freeDown() ) {
 			// TODO: update View !
 			a.goDown();
-
-			// TODO: set Sim Event with cells that gets empty afterwards
+			// isMoving=true;
+			// TODO: set Sim Event -> ACTOR_AFTER_MOVE
 
 			return true;
 		}
@@ -16,15 +16,30 @@ class ActorSim {
 		return false;
 
 	}
+	public static inline function tryFallDownLeft(a:IActor):Bool {return false;}
+	public static inline function tryFallDownRight(a:IActor):Bool {return false;}
 
+	// TODO: grid-swapping -> gridView index-swapping to !
 
 
 
 	public static inline function onAddToGrid(a:IActor) {
 		trace("onAddToGrid");
+		// TODO: add actor to View !
+		// tryFallDown(a);
 	}
 
 	public static inline function onAfterMove(a:IActor) {
 		trace("onAfterMove");
+		// isMoving=false;
+
+		// TODO: check the cells that was getting empty after move
+		// loop throught them and check:
+		//    a) if cell is empty (no other still get into that place while iterating)
+		//    b) trigger an actor not twice
+		//          by check actors "isMoving"-flag AND
+		//          by store the already-checked in a map to not trigger double (.clear afterwards) 
+		// 1) the upper ones
+		// 2) the left and right upper outsides
 	}
 }
